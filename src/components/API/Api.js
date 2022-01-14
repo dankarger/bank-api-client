6b45c0a02b6239ab8986898ffb2c3133083e2056
dankarger <karger.dan@gmail.com>
2022-01-14 19:36:26 +0200

    frist commit

--- src/components/API/Api.js
+++ src/components/API/Api.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 export const getUsers = async ()=> {
     try{
-        const users = await axios.get('http://127.0.0.1:3000/users',{
+        const users = await axios.get('http://127.0.0.1:3000/',{
             headers: {
-                'Content-Type': 'application/json'
+                'Access-Control-Allow-Origin': 'http://127.0.0.1:3001/',
+                 'Access-Control-Allow-Credentials': 'true'
             }
         })
         console.log('users',users)
@@ -16,7 +17,7 @@ export const getUsers = async ()=> {
 }
 
 export const addUser=(user)=> {
-    axios.post('http://127.0.0.1:3000/users', {"user":user}, {
+    axios.post('http://127.0.0.1:3000', {"user":user}, {
             headers: {
                 'Content-Type': 'application/json'
             }
